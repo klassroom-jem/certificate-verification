4d560d5ebce2668a439cdfe8a48324b9f48e88d0
klassroom-jem <klassroom.jem@gmail.com>
2026-05-15 12:03:54 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+const certificates = {
+  "FKEL-MLE-OJT-2026-001-DMP1": {
+    name: "Shreyash Mishra",
+    program: "Fusion Klassroom Edutech LTD OJT Internship",
+    issueDate: "15 May 2026",
+    status: "Valid"
+  },
+  "FKEL-MLE-OJT-2026-002-77ZK": {
+    name: "Nandini Pathak",
+    program: "Fusion Klassroom Edutech LTD OJT Internship",
+    issueDate: "15 May 2026",
+    status: "Valid"
+  }
+};
